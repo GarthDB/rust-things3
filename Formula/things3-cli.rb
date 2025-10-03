026abfabfe924f9de966ae985b09acd4875b2e06
garthdb <garthdb@gmail.com>
2025-10-03 11:32:37 -0600

    chore: update Homebrew formula with correct SHA256 for v0.2.0

--- Formula/things3-cli.rb
+++ Formula/things3-cli.rb
@@ -2,7 +2,7 @@ class Things3Cli < Formula
   desc "CLI tool for Things 3 with integrated MCP server"
   homepage "https://github.com/GarthDB/rust-things3"
   url "https://github.com/GarthDB/rust-things3/archive/v0.2.0.tar.gz"
-  sha256 "" # This will need to be filled in after the release
+  sha256 "43cbe9ab83ef4c650ecfa9e30ad529762820ef204179b1b498e61babd9e11b5f" # This will need to be filled in after the release
   license "MIT"
   head "https://github.com/GarthDB/rust-things3.git", branch: "main"
 
